4016ef84ac4e8e1e2a4b8422dafd0767cf8bc675
ajioka2310 <fumito0626@gmail.com>
2026-07-02 12:04:28 +0900

    add macro

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/via_fix/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/via_fix/keymap.c
@@ -181,9 +181,8 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             SEND_STRING(SS_TAP(X_LALT) SS_TAP(X_H) SS_TAP(X_G) SS_TAP(X_K));
         }
       return false;
-
-      return true;
     }
+    return true;
 }
 // ==========================================
 // 2. キーマップの配置（TAP_0 の定義より後ろに置く）
