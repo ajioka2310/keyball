79a29f6d460d99427e1ca959c8eadddb53ad7867
ajioka2310 <fumito0626@gmail.com>
2026-07-02 14:38:50 +0900

    add macro

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/via_fix/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/via_fix/keymap.c
@@ -265,7 +265,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB   , KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,                                        KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     , KC_BSPC  ,
     TD_ALT_GRV  , KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,                       KC_H     , KC_J     , KC_K     , KC_L     , KC_SCLN  , KC_ENT  ,
     KC_LSFT  , KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     ,                                        KC_N     , KC_M     , KC_COMM  , KC_DOT   , KC_SLSH  , MO(4)  ,
-               KC_LGUI  , MO(4)    , TAP_2  , KC_SPC   ,  KC_BTN1  ,                                        MO(1),MO(2), RCTL_T(KC_LNG2),     KC_RALT  , MO(3)
+               KC_LGUI  , MO(4)    , TAP_2  , KC_SPC   ,  KC_BTN1  ,                                        LT(1,KC_EQL),LT(2,KC_MINS), _______, _______  , LT(3,KC_QUOT)
   ),
 
   [1] = LAYOUT_universal(
